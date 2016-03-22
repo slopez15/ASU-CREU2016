ae477ca680a5432b26d2f8b66632f207f6ccc16f
Christopher Diaz <crdiaz3@asu.edu>
2016-03-22 15:31:44 -0700

    Modulized code, and now storing cities that resulted in a fault or did not contain any properties

--- Scrapers/Real-Estate/redfin_downloader.py
+++ Scrapers/Real-Estate/redfin_downloader.py
@@ -5,6 +5,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import csv
+
 download_path = os.getcwd() + "\\csv_data\\"
 fp = webdriver.FirefoxProfile("C:/Users/Chris/AppData/Roaming/Mozilla/Firefox/Profiles/lezp5kwl.selenium")
 fp.set_preference("browser.download.folderList", 2)
@@ -15,7 +16,10 @@ fp.set_preference("browser.helperApps.neverAsk.saveToDisk","text/csv");
 browser = webdriver.Firefox(firefox_profile=fp)
 browser.get("https://www.redfin.com")
 
-fault_list = []
+def append_to_fault_file(newCity): #reference: http://pymotw.com/2/csv/
+    fd = open('fault_file.csv','a')
+    fd.write('%s, %s\n' % newCity)
+    fd.close()
 
 def rename_file(docName):
     os.chdir(download_path)
@@ -24,38 +28,43 @@ def rename_file(docName):
     files.sort(key=lambda x: os.path.getmtime(x))
     newest_file = files[-1]
     if "redfin" in newest_file:
-        os.rename(newest_file, docName+".csv")
+        newFilename = '%s, %s' % cityState
+        os.rename(newest_file, newFilename+".csv")
     else:
-        fault_list.append(docName)
+        append_to_fault_file('%s, %s' % cityState)
 
-with open('cities.csv', "r") as f:
-    data = csv.reader(f)
+def collect_csv_data():
+    with open('cities.csv', "r") as f:
+        data = csv.reader(f)
 
-    for row in data:
-        if row[0] != 'AL':  # Filter script by state
-            print('Not AL')
-        else:
-            elem = WebDriverWait(browser, 100).until(EC.presence_of_element_located((By.CLASS_NAME, "search-input-box")))
-            keys = row[2] + ', ' + row[0]
-
-            elem.clear()
-            elem.send_keys(keys)
-            elem.submit()
-            browser.implicitly_wait(10)
-
-            # Check for an unrecognized city
-            if browser.current_url == 'https://www.redfin.com/':
-                print('No listings for '+ keys)
-                fault_list.append(keys)
+        for row in data:
+            if row[0] != 'AL':  # Filter script by state
+                print('Not AL')
+            
             else:
-                # find download link. Throw excepion for a city with no listings
-                try:
-                    elem = WebDriverWait(browser, 100).until(EC.presence_of_element_located((By.CLASS_NAME, "downloadLink")))
-                    elem.click()
-                    rename_file(row[2] + '_' + row[0])
-                except NoSuchElementException:
-                    print('Empty Page for '+ keys)
-                    fault_list.append(keys)
+                elem = WebDriverWait(browser, 100).until(EC.presence_of_element_located((By.CLASS_NAME, "search-input-box")))
+                cityState = (row[2], row[0])
+                keys = '%s, %s' % cityState
+                elem.clear()
+                elem.send_keys(keys)
+                elem.submit()
+                browser.implicitly_wait(10)
 
-            browser.implicitly_wait(10)
-            browser.get("https://www.redfin.com")
+                # Check for an unrecognized city
+                if browser.current_url == 'https://www.redfin.com/':
+                    print('No listings for '+ keys)
+                    append_to_fault_file(cityState)
+                else:
+                    # find download link. Throw excepion for a city with no listings
+                    try:
+                        elem = WebDriverWait(browser, 100).until(EC.presence_of_element_located((By.CLASS_NAME, "downloadLink")))
+                        elem.click()
+                        rename_file(cityState)
+                    except NoSuchElementException:
+                        print('Empty Page for '+ keys)
+                        append_to_fault_file(cityState)
+
+                browser.implicitly_wait(10)
+                browser.get("https://www.redfin.com")
+
+collect_csv_data()
