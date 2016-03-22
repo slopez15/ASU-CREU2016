9f985d8230b5d91aeadcfab3c923964da37763cf
Christopher Diaz <crdiaz3@asu.edu>
2016-03-22 13:45:06 -0700

    Added feature to rename the files after they are downloaded

--- Scrapers/Real-Estate/redfin_downloader.py
+++ Scrapers/Real-Estate/redfin_downloader.py
@@ -6,14 +6,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import csv
 
-#os.chdir(SAVE_TO_DIRECTORY)
-#files = filter(os.path.isfile, os.listdir(SAVE_TO_DIRECTORY))
-#files = [os.path.join(SAVE_TO_DIRECTORY, f) for f in files] # add path to each file
-#files.sort(key=lambda x: os.path.getmtime(x))
-#newest_file = files[-1]
-#os.rename(newest_file, docName+".pdf")
-
-
 fp = webdriver.FirefoxProfile("C:/Users/Chris/AppData/Roaming/Mozilla/Firefox/Profiles/lezp5kwl.selenium")
 fp.set_preference("browser.download.manager.showWhenStarting",False)
 fp.set_preference("browser.helperApps.neverAsk.saveToDisk","text/csv");
@@ -23,13 +15,23 @@ browser.get("https://www.redfin.com")
 
 fault_list = []
 
+def rename_file(docName):
+    os.chdir("C:/Users/Chris/Downloads/")
+    files = filter(os.path.isfile, os.listdir("C:/Users/Chris/Downloads/"))
+    files = [os.path.join("C:/Users/Chris/Downloads/", f) for f in files] # add path to each file
+    files.sort(key=lambda x: os.path.getmtime(x))
+    newest_file = files[-1]
+    if "redfin" in newest_file:
+        os.rename(newest_file, docName+".csv")
+    else:
+        fault_list.append(docName)
 
 with open('cities.csv', "r") as f:
     data = csv.reader(f)
 
     for row in data:
-        if row[0] != 'AZ':  # Filter script by state
-            print('Not AZ')
+        if row[0] != 'AL':  # Filter script by state
+            print('Not AL')
         else:
             elem = WebDriverWait(browser, 100).until(EC.presence_of_element_located((By.CLASS_NAME, "search-input-box")))
             keys = row[2] + ', ' + row[0]
@@ -37,16 +39,18 @@ with open('cities.csv', "r") as f:
             elem.clear()
             elem.send_keys(keys)
             elem.submit()
+            browser.implicitly_wait(10)
 
             # Check for an unrecognized city
             if browser.current_url == 'https://www.redfin.com/':
                 print('No listings for '+ keys)
                 fault_list.append(keys)
             else:
-                # Check for a city with no listings
+                # find download link. Throw excepion for a city with no listings
                 try:
-                    elem = WebDriverWait(browser, 100).until(EC.presence_of_element_located((By.CLASS_NAME, "downnloadLink")))
+                    elem = WebDriverWait(browser, 100).until(EC.presence_of_element_located((By.CLASS_NAME, "downloadLink")))
                     elem.click()
+                    rename_file(row[2] + '_' + row[0])
                 except NoSuchElementException:
                     print('Empty Page for '+ keys)
                     fault_list.append(keys)
