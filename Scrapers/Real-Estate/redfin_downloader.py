d23c5c113772f3135093115ae4a2e98acc1acdf2
Christopher Diaz <crdiaz3@asu.edu>
2016-03-22 14:44:05 -0700

    Adjusted code so files are downloaded to a relative path, so we all can run the script

--- Scrapers/Real-Estate/redfin_downloader.py
+++ Scrapers/Real-Estate/redfin_downloader.py
@@ -5,9 +5,11 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import csv
-
+download_path = os.getcwd() + "\\csv_data\\"
 fp = webdriver.FirefoxProfile("C:/Users/Chris/AppData/Roaming/Mozilla/Firefox/Profiles/lezp5kwl.selenium")
+fp.set_preference("browser.download.folderList", 2)
 fp.set_preference("browser.download.manager.showWhenStarting",False)
+fp.set_preference("browser.download.dir", download_path)
 fp.set_preference("browser.helperApps.neverAsk.saveToDisk","text/csv");
 
 browser = webdriver.Firefox(firefox_profile=fp)
@@ -16,9 +18,9 @@ browser.get("https://www.redfin.com")
 fault_list = []
 
 def rename_file(docName):
-    os.chdir("C:/Users/Chris/Downloads/")
-    files = filter(os.path.isfile, os.listdir("C:/Users/Chris/Downloads/"))
-    files = [os.path.join("C:/Users/Chris/Downloads/", f) for f in files] # add path to each file
+    os.chdir(download_path)
+    files = filter(os.path.isfile, os.listdir(download_path))
+    files = [os.path.join(download_path, f) for f in files] # add path to each file
     files.sort(key=lambda x: os.path.getmtime(x))
     newest_file = files[-1]
     if "redfin" in newest_file:
